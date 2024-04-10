d34327389498451a92966bc6d529eb8df2101718
~~Leo <ardileon444@gmail.com>
2024-04-10 19:18:43 +0700

    Learn Auth

---
+++ src/test/java/com/simpleAuth/bljrAuth/BljrAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleAuth.bljrAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BljrAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
